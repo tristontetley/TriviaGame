fe9566eeb6845fc945a83ac94fa7e211ad7277e3
Triston Tetley <tetleylax@aol.com>
2019-10-30 10:00:06 -0400

    i added to my html and am manipulating it with the dom

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,42 @@
-$(document).ready(function (){
+$(document).ready(function () {
 
-    
+  $("#trivia-content").hide();
+  $("#results-heading").hide();
+
+
+
+  $("#start-button").on("click", function () {
+    $("#trivia-content").show();
+    $("#starting-heading").hide();
+    timeLeft();
+
+  })
+
+  $("#finish-button").on("click", function () {
+    $("#results-heading").show();
+    $("#trivia-content").hide();
+    clearInterval(intervalId);
+  })
+
+  let intervalId;
+  let time = 10;
+
+  function timeLeft() {
+    intvalId = setInterval(count, 1000);
+  };
+
+  function count() {
+    time--;
+    console.log(time);
+    $("#time-left").text(time);
+    if(time === 0 ) {
+      clearInterval(intervalId);
+      $("#results-heading").show();
+      $("#trivia-content").hide();
+    };
+  };
+
+
+
+
 })
