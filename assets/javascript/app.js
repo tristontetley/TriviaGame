ca4c8826d886d53efee011bc06217e38b847982e
Triston Tetley <tetleylax@aol.com>
2019-10-30 19:37:11 -0400

    got the counter to work, working on how to change results numbers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,33 +10,52 @@ $(document).ready(function () {
     $("#starting-heading").hide();
     timeLeft();
 
-  })
+  });
 
   $("#finish-button").on("click", function () {
     $("#results-heading").show();
     $("#trivia-content").hide();
     clearInterval(intervalId);
-  })
+  });
 
   let intervalId;
-  let time = 10;
+  let time = 80;
 
   function timeLeft() {
-    intvalId = setInterval(count, 1000);
-  };
+    intervalId = setInterval(count, 1000);
+  }
 
   function count() {
     time--;
     console.log(time);
     $("#time-left").text(time);
-    if(time === 0 ) {
+    if (time === 0) {
       clearInterval(intervalId);
       $("#results-heading").show();
       $("#trivia-content").hide();
-    };
-  };
+    }
+  }
 
+  const correct = $(".correct")
+  let correctAnswer = 0;
+  let incorrectAnswer = 0;
+  let noAnswer = 8;
 
+  // $("#star-wars-form input").on("click", function () {
+  //   $("input[type=radio]:checked", "#star-wars-form");
+  //   noAnswer--;
+  //   $("#unanswered-question").text(noAnswer);
+    
+  // });
+$("#star-wars-form").click(function () {
+  $(".form-check-label").prop("checked", true);
+  console.log("hello");
+});
+
+//   $(".form-check-label").click(function () { 
+//     $(".correct").prop("checked", true);
+//     console.log("close");
+// });
 
 
 })
